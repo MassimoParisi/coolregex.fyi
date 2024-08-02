592cf471f4da1d9ad0c1776090d81fbe3f7dd00e
MassimoParisi <massimop98@outlook.it>
2024-08-02 18:42:25 +0200

    change title, description and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { CommandBar } from "@/components/custom/command-bar";
 import { RegexCard } from "@/components/custom/RegexCard";
 import { regexInfo } from "@/lib/regex_info";
 import { motion } from "framer-motion";
+import { Regex } from "lucide-react";
 import { useState } from "react";
 
 const container = {
@@ -31,12 +32,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="p-4 w-full">
-          <h1 className="text-3xl text-center font-bold mt-8 md:-mt-16 mb-8 w-full font-mono">
-            coolregex.
-            <span className="underline underline-offset-4 decoration-wavy decoration-yellow-300">
-              fyi
-            </span>
-          </h1>
+          <div className="flex items-center justify-center gap-4 text-center font-bold mt-8 md:-mt-16 mb-8 w-full font-mono">
+            <Regex className="w-8 h-8 text-black border-2 border-yellow-300 rounded-md p-0.5" />
+            <h1 className="text-3xl">
+              coolregex.
+              <span className="underline underline-offset-4 decoration-wavy decoration-yellow-300">
+                fyi
+              </span>
+            </h1>
+          </div>
           <CommandBar
             search={search}
             setSearch={setSearch}
