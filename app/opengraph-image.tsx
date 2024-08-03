374047ce5ee9bf50fd23da56b920ca40b07080ef
MassimoParisi <massimop98@outlook.it>
2024-08-03 10:31:40 +0200

    improve og

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,10 +1,11 @@
+import { Regex } from "lucide-react";
 import { ImageResponse } from "next/og";
 
 // Route segment config
 export const runtime = "edge";
 
 // Image metadata
-export const alt = "About Acme";
+export const alt = "coolregex.fyi";
 export const size = {
   width: 1200,
   height: 630,
@@ -19,8 +20,8 @@ export default async function Image() {
       // ImageResponse JSX element
       <div
         style={{
+          backgroundColor: "#FEF9C3",
           fontSize: 128,
-          background: "red",
           width: "100%",
           height: "100%",
           display: "flex",
@@ -28,22 +29,47 @@ export default async function Image() {
           justifyContent: "center",
         }}
       >
-        About Acme
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "16px",
+            textAlign: "center",
+            fontWeight: 700,
+            width: "100%",
+          }}
+          //   className="font-mono"
+        >
+          <Regex
+            style={{
+              width: "32px",
+              height: "32px",
+              border: "2px solid #FFD700",
+              borderRadius: "0.375rem",
+              padding: "0.125rem",
+            }}
+          />
+          <h1
+            style={{
+              fontSize: "1.875rem",
+              lineHeight: "2.25rem",
+            }}
+          >
+            coolregex.
+            <span
+              style={{
+                textDecoration: "underline",
+                textDecorationColor: "#FFD700",
+                textDecorationStyle: "wavy",
+                textUnderlineOffset: "4px",
+              }}
+            >
+              fyi
+            </span>
+          </h1>
+        </div>
       </div>
-    ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
-      //   fonts: [
-      //     {
-      //       name: "Inter",
-      //       data: await interSemiBold,
-      //       style: "normal",
-      //       weight: 400,
-      //     },
-      //   ],
-    }
+    )
   );
 }
